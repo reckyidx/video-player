bfb6e428ce487d59870ac8673624d653f75506b1
reckyidx <rakhesh@ghoshak.com>
2022-08-21 00:36:43 +0530

    added icons for video controls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { useEffect, useRef } from "react";
+import PauseSharpIcon from "@mui/icons-material/PauseSharp";
+import Forward10SharpIcon from "@mui/icons-material/Forward10Sharp";
+import PlayArrowSharpIcon from "@mui/icons-material/PlayArrowSharp";
+import Replay10SharpIcon from "@mui/icons-material/Replay10Sharp";
 
 function App() {
   const videoRef = useRef(null);
@@ -11,10 +15,21 @@ function App() {
       <div className="videoContainer">
         <video
           ref={videoRef}
-          // src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
-          // type="video/mp4"
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+          type="video/mp4"
           className="videoPlayer"
         ></video>
+        <div className="controls">
+          <div className="rewind">
+            <Replay10SharpIcon fontSize="large" style={{ color: "#fff" }} />
+          </div>
+          <div className="play">
+            <PlayArrowSharpIcon fontSize="large" style={{ color: "#fff" }} />
+          </div>
+          <div className="forward">
+            <Forward10SharpIcon fontSize="large" style={{ color: "#fff" }} />
+          </div>
+        </div>
       </div>
     </>
   );
